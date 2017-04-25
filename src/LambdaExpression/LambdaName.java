461e5270dd3704fe9268c905189ade1ba946d3ca
sunandapatra <sunandapatra16@gmail.com>
2017-04-24 23:45:42 -0400

    Add files via upload

---
+++ src/LambdaExpression/LambdaName.java
@@ -0,0 +1,15 @@
+package LambdaExpression;
+
+public class LambdaName {
+
+	final static String salutation = "Hello! ";
+
+	public static void main(String args[]) {
+		GreetingService greetService1 = message -> System.out.println(salutation + message);
+		greetService1.sayMessage("Mahesh");
+	}
+
+	interface GreetingService {
+		void sayMessage(String message);
+	}
+}
