b0b373e381b229892e718db34ea8493749541eea
sunandapatra <sunandapatra16@gmail.com>
2017-04-24 23:46:24 -0400

    Add files via upload

---
+++ src/UserInterface/UserInterface1.java
@@ -0,0 +1,26 @@
+package UserInterface;
+
+import java.awt.Button;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class UserInterface1 extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Button btn = new Button("Click Me");
+		StackPane root = new StackPane();
+		//root.getChildren().add(btn);
+		Scene scene = new Scene(root, 500, 300);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+}
