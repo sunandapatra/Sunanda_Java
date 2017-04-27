a5a01e43ae7dbb95dce3868e4d99dee2593b3a18
sunandapatra <sunandapatra16@gmail.com>
2017-04-26 20:58:33 -0400

    Add files via upload

---
+++ src/Domain/Employee.java
@@ -0,0 +1,26 @@
+package Domain;
+
+public class Employee {
+	private String firstName;
+	private String lastName;
+	private String gender;
+	
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
